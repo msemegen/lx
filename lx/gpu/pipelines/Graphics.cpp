77c34569e8fc82080e5e873bae8c514b9579f027
Mateusz Semegen <mateusz.semegen@gmail.com>
2025-05-08 22:45:54 +0200

    WIP

--- lx/gpu/pipelines/Graphics.cpp
+++ lx/gpu/pipelines/Graphics.cpp
@@ -2,10 +2,32 @@
 #include <lx/gpu/pipelines/Graphics.hpp>
 
 namespace lx::gpu::pipelines {
+using namespace lx::containers;
+
 void Graphics::create(const Properties& properties_a)
 {
+    Vector<VkVertexInputBindingDescription> vk_vertex_input_binding_descriptions(properties_a.vertex_input.bindings.size());
+    Vector<VkVertexInputAttributeDescription> vk_vertex_input_attribute_descriptions(properties_a.vertex_input.attributes.size());
+
+    for (const auto& vertex_binding : properties_a.vertex_input.bindings)
+    {
+        vk_vertex_input_binding_descriptions.emplace_back(
+            vertex_binding.binding, vertex_binding.stride, static_cast<VkVertexInputRate>(vertex_binding.rate));
+    }
+    for (const auto& vertex_attribute : properties_a.vertex_input.attributes)
+    {
+        vk_vertex_input_attribute_descriptions.emplace_back(
+            vertex_attribute.location, vertex_attribute.binding, static_cast<VkFormat>(vertex_attribute.format), vertex_attribute.offset);
+    }
+
     VkPipelineVertexInputStateCreateInfo vk_pipeline_vertex_input_state_create_info {
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO, .pNext = nullptr, .flags = 0u
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0u,
+        .vertexBindingDescriptionCount = static_cast<std::uint32_t>(properties_a.vertex_input.bindings.size()),
+        .pVertexBindingDescriptions = vk_vertex_input_binding_descriptions.get_buffer(),
+        .vertexAttributeDescriptionCount = static_cast<std::uint32_t>(properties_a.vertex_input.attributes.size()),
+        .pVertexAttributeDescriptions = vk_vertex_input_attribute_descriptions.get_buffer()
     };
 
     VkPipelineInputAssemblyStateCreateInfo vk_pipeline_input_assembly_state_create_info {
@@ -23,6 +45,7 @@ void Graphics::create(const Properties& properties_a)
     VkGraphicsPipelineCreateInfo vk_graphics_pipeline_create_info { .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
                                                                     .pNext = nullptr,
                                                                     .flags = 0u,
+                                                                    .pVertexInputState = &vk_pipeline_vertex_input_state_create_info,
                                                                     .pInputAssemblyState = &vk_pipeline_input_assembly_state_create_info,
                                                                     .pColorBlendState = &vk_pipeline_color_blend_state_create_info };
 }
